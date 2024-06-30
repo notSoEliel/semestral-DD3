ca91aa86bffe85b224024b0af5c9f812894160b9
Eliel García <bgt.eliel@gmail.com>
2024-06-30 15:28:27 -0500

    getters y setters supongo

--- src/app/Investigador.java
+++ src/app/Investigador.java
@@ -9,12 +9,52 @@ public class Investigador {
     private String inveCodigo;
     private LinkedList<Publicaciones> publicaciones;
 
-    public Investigador(int inveId, String inveNombre, String inveArea, String inveCodigo,
-            LinkedList<Publicaciones> publicaciones) {
+    public Investigador(int inveId, String inveNombre, String inveArea, String inveCodigo, LinkedList<Publicaciones> publicaciones) {
         this.inveId = inveId;
         this.inveNombre = inveNombre;
         this.inveArea = inveArea;
         this.inveCodigo = inveCodigo;
+        this.publicaciones = publicaciones;
+    }
+
+    // Getters y setters
+    public int getInveId() {
+        return inveId;
+    }
+
+    public void setInveId(int inveId) {
+        this.inveId = inveId;
+    }
+
+    public String getInveNombre() {
+        return inveNombre;
+    }
+
+    public void setInveNombre(String inveNombre) {
+        this.inveNombre = inveNombre;
+    }
+
+    public String getInveArea() {
+        return inveArea;
+    }
+
+    public void setInveArea(String inveArea) {
+        this.inveArea = inveArea;
+    }
+
+    public String getInveCodigo() {
+        return inveCodigo;
+    }
+
+    public void setInveCodigo(String inveCodigo) {
+        this.inveCodigo = inveCodigo;
+    }
+
+    public LinkedList<Publicaciones> getPublicaciones() {
+        return publicaciones;
+    }
+
+    public void setPublicaciones(LinkedList<Publicaciones> publicaciones) {
         this.publicaciones = publicaciones;
     }
 }
--- src/app/Proyectos.java
+++ src/app/Proyectos.java
@@ -18,4 +18,61 @@ public class Proyectos {
         this.proyFechaFin = proyFechaFin;
         this.proyDescripcion = proyDescripcion;
     }
+
+    // Getters y setters
+    public int getProyId() {
+        return proyId;
+    }
+
+    public void setProyId(int proyId) {
+        this.proyId = proyId;
+    }
+
+    public String getProyCodigo() {
+        return proyCodigo;
+    }
+
+    public void setProyCodigo(String proyCodigo) {
+        this.proyCodigo = proyCodigo;
+    }
+
+    public String getProyNombre() {
+        return proyNombre;
+    }
+
+    public void setProyNombre(String proyNombre) {
+        this.proyNombre = proyNombre;
+    }
+
+    public String getProyHorasDedicacion() {
+        return proyHorasDedicacion;
+    }
+
+    public void setProyHorasDedicacion(String proyHorasDedicacion) {
+        this.proyHorasDedicacion = proyHorasDedicacion;
+    }
+
+    public String getProyFechaInicio() {
+        return proyFechaInicio;
+    }
+
+    public void setProyFechaInicio(String proyFechaInicio) {
+        this.proyFechaInicio = proyFechaInicio;
+    }
+
+    public String getProyFechaFin() {
+        return proyFechaFin;
+    }
+
+    public void setProyFechaFin(String proyFechaFin) {
+        this.proyFechaFin = proyFechaFin;
+    }
+
+    public String getProyDescripcion() {
+        return proyDescripcion;
+    }
+
+    public void setProyDescripcion(String proyDescripcion) {
+        this.proyDescripcion = proyDescripcion;
+    }
 }
