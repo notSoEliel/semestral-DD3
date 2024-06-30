1b1ae3a796f91a06dabe7772cb8c17036f95b285
Eliel García <bgt.eliel@gmail.com>
2024-06-30 15:27:04 -0500

    manejo de errores y cerrar conexión

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -1,21 +1,31 @@
 package config;
-import java.sql.*;
-public class Conexion {
-    private String  usuario= "root";
-    private String password = "vivapythonnophp";
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private String usuario = "root";
+    private String password = "vivapythonnophp";
     private Connection cnn;
 
-    // Método de conexion
+    // Método de conexión
     public Connection establecerConexion() throws Exception {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             cnn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=investigacion;encrypt=false;trustServerCertificate=false", usuario, password);
             return cnn;
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Para el programador: " + e + "\n\nPara el usuario: Error... No se pudo cargar el driver de SQL Server");
+        } catch (SQLException e) {
+            throw new Exception("Para el programador: " + e + "\n\nPara el usuario: Error... No se pudo establecer la conexión");
         }
-        catch (SQLException e){
-            throw new Exception ("\n" + e +
-                    "\n\nPara el usuario: Error... No se pudo establecer la conexion");
+    }
+
+    // Método para cerrar la conexión
+    public void cerrarConexion() throws SQLException {
+        if (cnn != null && !cnn.isClosed()) {
+            cnn.close();
         }
     }
 }
