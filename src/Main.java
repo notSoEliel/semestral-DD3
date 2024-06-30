3be766e527ee4e13684618a63548a8357ff4af7e
Eliel García <bgt.eliel@gmail.com>
2024-06-30 15:26:14 -0500

    try catch main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import config.Conexion;
-
 import java.sql.Connection;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Conexion obj = new Conexion();
-
-        Connection cnn = obj.establecerConexion();
-
-        System.out.println(":D");
+    public static void main(String[] args) {
+        try {
+            Conexion obj = new Conexion();
+            Connection cnn = obj.establecerConexion();
+            System.out.println(":D"); //Para mí para saber que se logró la conexión xd
+        } catch (Exception e) {
+            e.printStackTrace(); // Para depuración en caso de error
+        }
     }
 }
